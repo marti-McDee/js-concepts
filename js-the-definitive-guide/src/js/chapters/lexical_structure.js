dd88cc88a19e559f6f46fdf491ccdd5a228a205a
Marti <marti@martimcdee.com>
2022-10-26 02:41:30 +0000

    The Text of a JS Program | Comments | Literals | Identifiers and Reserved Words | Unicode | Optional Semicolons

--- js-the-definitive-guide/src/js/chapters/lexical_structure.js
+++ js-the-definitive-guide/src/js/chapters/lexical_structure.js
@@ -5,4 +5,4 @@
 const señor = 'David'
 const señora = 'Maria'
 
-console.log('se\u{00f1}or', 'y', 'se\u00f1ora', ' ', '\u{1f603}')
+// console.log('se\u{00f1}or', 'y', 'se\u00f1ora', ' ', '\u{1f603}')
