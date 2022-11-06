c786e74c350988386a4eed42a098374688a64f91
Marti <marti@martimcdee.com>
2022-11-06 23:15:21 +0000

    Async Programming with Callbacks | Implementing Iterable Objects | Generators | Advanced Generator Features

--- js-the-definitive-guide/src/js/chapters/asynchronous.js
+++ js-the-definitive-guide/src/js/chapters/asynchronous.js
@@ -1,9 +1,9 @@
 // Chapter 13: Asynchronous JavaScript
 
-/* Using asynchronous JavaScript (such as callbacks, promises, and async/await), you can perform long network requests
+/* Using asynchronous JavaScript (such as cbs, promises, and async/await), you can perform long network requests
  without blocking the main thread. */
 
-/** 13.1 Asynchronous Programming with Callbacks **/
+/** 13.1 Asynchronous Programming with cbs **/
 
 /* Timers */
 
@@ -53,35 +53,87 @@ third()
 // second
 
 /* Events */
-let okay = document.getElementById('confirm')
-const p = document.getElementById('my-paragraph')
-okay.addEventListener('pointerdown', function () {
-  // console.log(this.innerHTML)
-})
+// let okay = document.getElementById('confirm')
+// const p = document.getElementById('my-paragraph')
+// okay.addEventListener('pointerdown', function () {
+//   // console.log(this.innerHTML)
+// })
 
 /* Network Events */
-const request = new XMLHttpRequest()
+// const request = new XMLHttpRequest()
 
 // send GET request to test API
-const getText = function (
-  callback // How can I use this callback?
-) {
-  var request = new XMLHttpRequest()
-  request.onreadystatechange = function () {
-    if (request.readyState == 4 && request.status == 200) {
-      callback(request.responseText) // Another callback here
-    }
-  }
-  const id = Math.floor(Math.random() * 5 + 1)
-  request.open('GET', `https://jsonplaceholder.typicode.com/users/${id}`)
-  request.send()
-}
+// const getText = function (
+//   cb // How can I use this cb?
+// ) {
+//   var request = new XMLHttpRequest()
+//   request.onreadystatechange = function () {
+//     if (request.readyState == 4 && request.status == 200) {
+//       cb(request.responseText) // Another cb here
+//     }
+//   }
+//   const id = Math.floor(Math.random() * 5 + 1)
+//   request.open('GET', `https://jsonplaceholder.typicode.com/users/${id}`)
+//   request.send()
+// }
 
 function cb(data) {
   p.innerText = data
 }
 
-const inner = document.getElementById('inner-box')
-inner.addEventListener('pointerover', _ => getText(cb))
+// const inner = document.getElementById('inner-box')
+// inner.addEventListener('pointerover', _ => getText(cb))
 
-/* Callbacks and Events in Node */
+/* cbs and Events in Node */
+const fs = require('fs')
+const { request } = require('http')
+let options = {
+  // default options should go here
+}
+
+// Read a config file, then call the cb function
+fs.readFile("../helper/config.json", "utf-8", (err, text) => {
+  if(err){
+    // display a warning, but continue
+    console.warn("Could not read config file: ", err)
+  } else {
+    // parse the file contents and assign to the options object
+    Object.assign(options, JSON.parse(text))
+  }
+
+  // start running the program
+  // startProgram(options)
+})
+
+// make an http request for the contents of a URL in node
+// Node uses an on() method to register event listeners, insted of addEventListener()
+const https = require("https")
+
+// Read the text content of the URL and asynchronously pass it to the cb
+function getText(url, cb){
+  // Start an http GET request for the URL
+  request = https.get(url)
+
+  // Register a function to handle the "response" event
+  request.on("response", response => {
+    // the response event means that the response headers have been received
+    let httpStatus = response.statusCode
+
+    // The body of the http response has not been received yet.
+    // So, we register more event handlers to be called when it arrives
+    response.setEncoding("utf-8")
+    let body = ""
+
+    // This event handler is called when a chunk of the body is ready
+    response.on("data", chunk => { body += chunk })
+
+    // This event handler is called when the response is complete
+    response.on("end", () => {
+      if(httpStatus === 200) cb(null, body)
+      else cb(httpStatus, null)
+    })
+  })
+
+  // We also register an event handler for the lower-level network errors
+  request.on("error", (err) => cb(err, null))
+}
