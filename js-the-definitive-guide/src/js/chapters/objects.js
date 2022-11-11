132c1dd9846b8796659982f2b068bba5b4f25d19
David Martinez <anddagger@gmail.com>
2022-11-10 21:20:51 -0600

    Creating Objects | Querying and Setting Properties | Deleting, Testing, and Enumerating Properties | Extending and Serializing Objects | Object Methods | Extended Object Literal Syntax

--- js-the-definitive-guide/src/js/chapters/objects.js
+++ js-the-definitive-guide/src/js/chapters/objects.js
@@ -4,8 +4,8 @@
 
 /** Object Literals **/
 let empty = {}
-let point = {x: 1, y: 0}
-let p2 = {x: point.x, y: point.y + 1}
+let point = { x: 1, y: 0 }
+let p2 = { x: point.x, y: point.y + 1 }
 
 // when property names include hyphens and spaces, use quotes
 let book = {
@@ -20,4 +20,20 @@ let book = {
 
 
 /** Prototypes **/
-console.log(p2.hasOwnProperty('z'))
+// console.log(p2.hasOwnProperty('z'))
+
+
+/** Object.create() **/
+
+// Object.create() creates a new object using its first argument as the prototype of that object:
+let o1 = Object.create({ x: 1, y: 2 })
+console.log(o1.x + o1.y)
+console.log(o1)
+
+// the example below inherits no props or methods (not even basic methods, like toString())
+let o2 = Object.create(null)
+console.log(o2)
+
+// creates an ordinary empty object
+let o3 = Object.create(Object.prototype)
+console.log(o3)
